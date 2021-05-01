668e44d271a41d4236c0f4b50a14d12842575631
Marta Parra Sáez <martaps216@gmail.com>
2021-05-01 13:32:05 +0200

    Maquetar formularios

--- VERSION 1/registro.php
+++ VERSION 1/registro.php
@@ -17,24 +17,58 @@ include 'secciones/nav.html';
 		<h1 class="title is-1">Bienvenido a Educalegre</h1>
 		<!--Añadimos la acción de formulario para verificar y dar de alta al usuario-->
 		<!--Verificar con javascript y una ventana, dar de alta con php insertando en una base de datos-->
-			<form class="form" action = "php/alta_usuario.php" method = "POST">
-				<div border = "1">
-					Tipo de usuario
-					<select name="tipo_usu_section_reg">
-						<option value="tipo_usu_alumno_reg">Alumno</option>
-						<option value="tipo_usu_profesor_reg">Profesor</option>
-					</select><br>
-					Nombre: <input type="text" name="usu_reg" /><br>
-					Apellidos: <input type="text" name="pass_reg" /><br>
-					Nombre usuario: <input type="text" name="nom_usu_reg" placeholder="nombre.apellidos@educalegre.com" /><br>
-					Contraseña: <input type="password" name="pass_usu_reg" placeholder="contraseña" /><br>
-					Confirmar contraseña: <input type="password" name="pass_usu_reg_confir" placeholder="contraseña" /><br>
-					E-mail: <input type="email" name="email_usu_reg" placeholder="usuario@gmail.com" /></br>
-					<input type="checkbox" name="terms_usu_reg">Terminos y condiciones</input></br>
-					<a href="#">Leer terminos y condiciones de uso</a></br>
-					<input type="submit" name="alta_user_reg" value="Darse de alta" />
-					<input type="reset" name="reseteo_datos_reg" value="Borrar" /><br>
-				</div>
+			<form class="form_reg" action = "php/alta_usuario.php" method = "POST">
+				<div class="columns">
+					<div class="column is-half">	<!--Primer mitad-->
+						<div class="field">
+							<label class="label">Tipo de usuario</label>
+							<div class="control">
+								<div class="select is-rounded">
+									<select name="tipo_usu_section_reg">
+										<option value = "tipo_usu_alumno_reg">Alumno</option>
+										<option value = "tipo_usu_profesor_reg">Profesor</option>
+									</select>
+								</div>
+							</div>
+						</div>
+						<div class = "field">
+							<label class = "label">Nombre</label> 
+							<input class = "input" type = "text" name = "nombre_reg" placeholder = "Tu nombre"/><br>
+						</div>
+						<div class = "field">
+							<label class = "label">Apellidos</label> 
+							<input class = "input" type = "text" name = "ape_reg" placeholder = "Tus apellidos"><br>
+						</div>
+						<div class = "field">
+							<label class = "label">Correo</label> 
+							<input class = "input" type = "email" name = "nom_usu_reg" placeholder = "usuario@gmail.com"/><br>
+						</div>
+					</div>
+					<div class="column">	<!--Segunda mitad-->
+						<div class = "field">
+							<label class = "label">Nombre usuario</label> 
+							<input class = "input" type = "text" name = "nom_usu_reg" placeholder = "nombre de usuario"/><br>
+						</div>
+						<div class = "field">
+							<label class = "label">Contraseña</label> 
+							<input class = "input" type = "password" name = "pass_reg" placeholder = "contraseña"><br>
+						</div>
+						<div class = "field">
+							<label class = "label">Confirmar contraseña</label> 
+							<input class = "input" type = "password" name = "pass_reg_confirm" placeholder = "contraseña"><br>
+						</div>
+						<div class="field">
+							<div class="control">
+								<label class="checkbox">
+									<input type="checkbox">He aceptado los <a href="#">Términos y condiciones de uso</a>
+								</label>
+							</div>
+						</div>
+						<div class = "field is-grouped">
+							<input class = "button is-link" type = "submit" name = "alta_user_reg" value = "Darse de alta"/>
+							<input class = "button is-link is-light" type = "reset" name = "reseteo_datos_reg" value = "Borrar"/><br>
+						</div>
+					</div>
 			</form>
 		</div>
 	</section>
--- VERSION 1/secciones/footer.php
+++ VERSION 1/secciones/footer.php
@@ -7,7 +7,7 @@
 		<?php
 		setlocale(LC_TIME,"es_ES");
 		#Correción del problema a la hora de mostrar la fecha en español
-		echo utf8_encode(strftime("Hoy es %A del mes %B "));
+		echo utf8_encode(strftime("Hoy es %A del mes de %B "));
 		echo strftime("del año %Y");
 		?>
 	</div>
