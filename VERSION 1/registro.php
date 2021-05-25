083677af53d016cda18d20ef6943849d1563cb98
Jeffrey Sanchez <62367457+ieffers@users.noreply.github.com>
2021-05-25 11:34:57 +0200

    Fix en Aspecto del formulario registro

--- VERSION 1/registro.php
+++ VERSION 1/registro.php
@@ -6,7 +6,7 @@
 	es debido.
 !-->
 <?php 
-//Mostrar contenido de header y nav
+//Mostrar contenido de header y nav2
 $title = 'Educalegre Registro';
 include 'secciones/header.php';
 include 'secciones/nav.html';
@@ -17,99 +17,107 @@ include 'secciones/nav.html';
 		<h1 class="title is-1">Bienvenido a Educalegre</h1>
 		<!--Añadimos la acción de formulario para verificar y dar de alta al usuario-->
 		<!--Verificar con javascript y una ventana, dar de alta con php insertando en una base de datos-->
-			<form id = "form_reg" class="box is-fluid" action = "php/alta_usuario.php" method = "POST">
-				<div class="columns is-mobile"> <!--Modificador in-mobile para que las columnas se muestren bien en móviles-->
-					<div class="column is-half">	<!--Primer mitad-->
-						<div class="field">
-							<label class="label">Tipo de usuario</label>
-							<div class="control">
-								<div class="select is-rounded">
-									<select name="tipo_usu_section_reg">
-										<option value = "tipo_usu_alumno_reg">Alumno</option>
-										<option value = "tipo_usu_profesor_reg">Profesor</option>
-									</select>
-								</div>
-							</div>
-						</div>
-						<div class = "field">
-							<label class = "label">Nombre</label> 
-							<input class = "input" type = "text" name = "nombre_reg" placeholder = "Tu nombre"/><br>
-						</div>
-						<div class = "field">
-							<label class = "label">Primer apellido</label> 
-							<input class = "input" type = "text" name = "ape1_reg" placeholder = "Tu primer apellido"><br>
-						</div>
-						<div class = "field">
-							<label class = "label">Segundo apellido</label> 
-							<input class = "input" type = "text" name = "ape2_reg" placeholder = "Tu segundo apellido"><br>
-						</div>
-						<div class = "field">
-							<label class = "label">DNI</label> 
-							<div class="control has-icons-right">
-								<input class = "input" type = "text" name = "dni_reg"/>
-								<span class="icon is-small is-right">
-			                		<i class="fas fa-envelope"></i>
-			            		</span>
-							</div>
+		<form id="form_reg" class="box is-fluid" action="php/alta_usuario.php" method="POST">
+			<div class="field">
+				<label class="label">Tipo de usuario</label>
+				<div class="control">
+					<div class="select is-rounded">
+						<select name="tipo_usu_section_reg">
+							<option value="tipo_usu_alumno_reg">Alumno</option>
+							<option value="tipo_usu_profesor_reg">Profesor</option>
+						</select>
+					</div>
+				</div>
+			</div>
+			<div class="columns is-mobile">
+				<!--Modificador in-mobile para que las columnas se muestren bien en móviles-->
+				<div class="column is-half">
+					<!--Primer mitad-->
+
+					<div class="field">
+						<label for="nombre_reg" class="label">Nombre</label>
+						<input class="input" type="text" name="nombre_reg" id="nombre_reg" placeholder="Tu nombre" /><br>
+					</div>
+					<div class="field">
+						<label for="ape1_reg" class="label">Primer apellido</label>
+						<input class="input" type="text" name="ape1_reg" id="ape1_reg" placeholder="Tu primer apellido"><br>
+					</div>
+					<div class="field">
+						<label for="ape2_reg" class="label">Segundo apellido</label>
+						<input class="input" type="text" name="ape2_reg" id="ape2_reg" placeholder="Tu segundo apellido"><br>
+					</div>
+				
+					<div class="field">
+						<label for="pass_reg" class="label">Contraseña</label>
+						<div class="control has-icons-right">
+							<input class="input" type="password" name="pass_reg" id="pass_reg" placeholder="Contraseña">
+							<span class="icon is-small is-right">
+								<i class="fas fa-key"></i>
+							</span>
 						</div>
 					</div>
-					<div class="column is-half">	<!--Segunda mitad-->
-						<div class = "field">
-							<label class = "label">Teléfono</label> 
-							<div class = "control has-icons-right">
-								<input class = "input" type = "text" name = "tel_reg" placeholder = "Fijo/móvil"/>
-								<span class="icon is-small is-right">
-			                		<i class="fas fa-phone"></i>
-			            		</span>
-							</div>
-						</div>
-						<div class = "field">
-							<label class = "label">Correo</label> 
-							<div class="control has-icons-right">
-								<input class = "input" type = "email" name = "correo_reg" placeholder = "usuario@educalegre.com"/>
-								<span class="icon is-small is-right">
-			                		<i class="fas fa-envelope"></i>
-			            		</span>
-							</div>
-						</div>
-						<div class = "field">
-							<label class = "label">Contraseña</label> 
-							<div class = "control has-icons-right">	
-								<input class = "input" type = "password" name = "pass_reg" placeholder = "contraseña">
-								<span class="icon is-small is-right">
-			                		<i class="fas fa-key"></i>
-			            		</span>
-							</div>
-						</div>
-						<div class = "field">
-							<label class = "label">Confirmar contraseña</label> 
-							<div class = "control has-icons-right">
-								<input class = "input" type = "password" name = "pass_reg_confirm" placeholder = "contraseña">
-								<span class="icon is-small is-right">
-			                		<i class="fas fa-key"></i>
-			            		</span>
-							</div>
-						</div>
-						<div class="field">
-							<div class="control">
-								<label class="checkbox">
-									<input type="checkbox">&nbsp;He aceptado los <a href="#">Términos y condiciones de uso</a>
-								</label>
-							</div>
-						</div>
-						<div class = "field is-grouped">
-							<input class = "button is-link" type = "submit" name = "alta_user_reg" value = "Darse de alta"/>
-							&nbsp;&nbsp;&nbsp;
-							<input class = "button is-link is-light" type = "reset" name = "reseteo_datos_reg" value = "Borrar"/><br>
+				</div>
+				<div class="column is-half">
+					<!--Segunda mitad-->
+					<div class="field">
+						<label class="label">DNI</label>
+						<div class="control has-icons-right">
+							<input class="input" type="text" name="dni_reg" placeholder="DNI/NIE" />
+							<span class="icon is-small is-right">
+								<i class="fas fa-envelope"></i>
+							</span>
 						</div>
 					</div>
-			</form>
-		</div>
+				
+					<div class="field">
+						<label class="label">Correo</label>
+						<div class="control has-icons-right">
+							<input class="input" type="email" name="correo_reg" placeholder="usuario@educalegre.com" />
+							<span class="icon is-small is-right">
+								<i class="fas fa-envelope"></i>
+							</span>
+						</div>
+					</div>
+					<div class="field">
+						<label class="label">Teléfono</label>
+						<div class="control has-icons-right">
+							<input class="input" type="text" name="tel_reg" placeholder="Fijo/móvil" />
+							<span class="icon is-small is-right">
+								<i class="fas fa-phone"></i>
+							</span>
+						</div>
+					</div>
+					<div class="field">
+						<label for="pass_reg_confirm" class="label">Confirmar contraseña</label>
+						<div class="control has-icons-right">
+							<input class="input" type="password" name="pass_reg_confirm" id="pass_reg_confirm" placeholder="Contraseña">
+							<span class="icon is-small is-right">
+								<i class="fas fa-key"></i>
+							</span>
+						</div>
+					</div>
+				
+				</div>
+			</div>
+			<div class="field">
+						<div class="control">
+							<label class="checkbox">
+								<input type="checkbox">&nbsp;He aceptado los <a href="#">Términos y condiciones de
+									uso</a>
+							</label>
+						</div>
+					</div>
+					<div class="field is-grouped">
+						<input class="button is-link" type="submit" name="alta_user_reg" value="Darse de alta" />
+						<input class="button is-link is-light" type="reset" name="reseteo_datos_reg" value="Borrar" /><br>
+					</div>
+		</form>
 	</section>
 </main>
-	<?php 
+<?php 
 		//Mostrar contenido de footer
 		include 'secciones/footer.php';
 	?>
-	</body>
+</body>
+
 </html>
--- VERSION 1/secciones/footer.php
+++ VERSION 1/secciones/footer.php
@@ -1,12 +1,7 @@
-<!--
-	MARTA PARRA SÁEZ
-	Archivo que contiene el pie de página de inicio
--->
 <footer>
 	<div>
 		<?php
 		setlocale(LC_TIME,"es_ES");
-		#Correción del problema a la hora de mostrar la fecha en español
 		echo utf8_encode(strftime("Hoy es %A del mes de %B "));
 		echo strftime("del año %Y");
 		?>
